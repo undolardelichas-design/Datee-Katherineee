f251181b3f202025d06675df8239a3225a88590f
undolardelichas-design <331829753+undolardelichas-design@users.noreply.github.com>
2026-09-22 16:33:18 -0600

    Agregar Vercel Analytics para ver las visitas

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
+import { inject } from '@vercel/analytics'
 import './index.css'
 import App from './App.tsx'
 
 gsap.registerPlugin(useGSAP)
+
+inject()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
